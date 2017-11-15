2f64e326f15299436d92658d969b85d78a366d7e
CDAGaming <cstack2011@yahoo.com>
2017-11-15 17:38:06 -0600

    Update Boost Compiled Files to 1_65_1

--- boost/move/detail/iterator_to_raw_pointer.hpp
+++ boost/move/detail/iterator_to_raw_pointer.hpp
@@ -7,8 +7,8 @@
 // See http://www.boost.org/libs/container for documentation.
 //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_CONTAINER_DETAIL_ITERATOR_TO_RAW_POINTER_HPP
-#define BOOST_CONTAINER_DETAIL_ITERATOR_TO_RAW_POINTER_HPP
+#ifndef BOOST_MOVE_DETAIL_ITERATOR_TO_RAW_POINTER_HPP
+#define BOOST_MOVE_DETAIL_ITERATOR_TO_RAW_POINTER_HPP
 
 #ifndef BOOST_CONFIG_HPP
 #  include <boost/config.hpp>
@@ -18,41 +18,42 @@
 #  pragma once
 #endif
 
-#include <boost/container/detail/iterator.hpp>
-#include <boost/container/detail/to_raw_pointer.hpp>
-#include <boost/intrusive/pointer_traits.hpp>
+#include <boost/move/detail/iterator_traits.hpp>
+#include <boost/move/detail/to_raw_pointer.hpp>
+#include <boost/move/detail/pointer_element.hpp>
 
 namespace boost {
-namespace container {
-namespace container_detail {
+namespace movelib {
+namespace detail {
 
 template <class T>
 inline T* iterator_to_pointer(T* i)
 {  return i; }
 
 template <class Iterator>
-inline typename boost::container::iterator_traits<Iterator>::pointer
+inline typename boost::movelib::iterator_traits<Iterator>::pointer
    iterator_to_pointer(const Iterator &i)
 {  return i.operator->();  }
 
 template <class Iterator>
 struct iterator_to_element_ptr
 {
-   typedef typename boost::container::iterator_traits<Iterator>::pointer      pointer;
-   typedef typename boost::intrusive::pointer_traits<pointer>::element_type   element_type;
+   typedef typename boost::movelib::iterator_traits<Iterator>::pointer  pointer;
+   typedef typename boost::movelib::pointer_element<pointer>::type      element_type;
    typedef element_type* type;
 };
 
+}  //namespace detail {
+
 template <class Iterator>
-inline typename iterator_to_element_ptr<Iterator>::type
+inline typename boost::movelib::detail::iterator_to_element_ptr<Iterator>::type
    iterator_to_raw_pointer(const Iterator &i)
 {
-   return ::boost::intrusive::detail::to_raw_pointer
-      (  ::boost::container::container_detail::iterator_to_pointer(i)   );
+   return ::boost::movelib::to_raw_pointer
+      (  ::boost::movelib::detail::iterator_to_pointer(i)   );
 }
 
-}  //namespace container_detail {
-}  //namespace container {
+}  //namespace movelib {
 }  //namespace boost {
 
-#endif   //#ifndef BOOST_CONTAINER_DETAIL_ITERATOR_TO_RAW_POINTER_HPP
+#endif   //#ifndef BOOST_MOVE_DETAIL_ITERATOR_TO_RAW_POINTER_HPP
